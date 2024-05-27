bbbbe7faf51d4ae48d8746318df1f23d4dd5ab89
Allv <wijayairawan123@gmail.com>
2024-05-28 02:30:30 +0700

    Feature: ready stock

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
 
 class Produk extends Model
 {
@@ -48,34 +49,67 @@ class Produk extends Model
     public function stok($date)
     {
         $totalSold = 0;
-        if ($this->jenis_produk == 'Cake') {
-            $productName = $this->nama_produk;
-            if (Str::contains($this->nama_produk, '1/2')) {
-                $productName = Str::before($this->nama_produk, ' (1/2 loyang)');
-            }
-            $totalSold =
-                DetailTransaksi::whereHas('produk', function ($query) use ($productName) {
-                    $query->where('nama_produk', 'like', $productName . '%');
-                })->whereHas('transaksi', function ($query) use ($date) {
-                    $query->whereDate('tanggal_ambil', $date);
-                    $query->where('status_transaksi', '!=', 'rejected');
-                })->get()->sum(function ($detailTransaksi) {
-                    if (!Str::contains($detailTransaksi->produk->nama_produk, '1/2')) {
-                        return $detailTransaksi->jumlah_item * 2;
-                    } else {
-                        return $detailTransaksi->jumlah_item;
-                    }
-                });
-            if (!Str::contains($this->nama_produk, '1/2')) {
-                $totalSold = ceil($totalSold / 2);
-            }
-        } else {
+        $dateInput = Carbon::parse($date);
+        $twoDaysFromNow = Carbon::now()->addDays(1);
+        if ($this->id_penitip != null) {
             $totalSold = $this->detailTransaksi()
                 ->whereHas('transaksi', function ($query) use ($date) {
-                    $query->whereDate('tanggal_ambil', $date);
                     $query->where('status_transaksi', '!=', 'rejected');
                 })
                 ->sum('jumlah_item');
+        } else {
+            if ($dateInput->greaterThan($twoDaysFromNow)) {
+                if ($this->jenis_produk == 'Cake') {
+                    $productName = $this->nama_produk;
+                    if (Str::contains($this->nama_produk, '1/2')) {
+                        $productName = Str::before($this->nama_produk, ' (1/2 loyang)');
+                    }
+                    $totalSold =
+                        DetailTransaksi::whereHas('produk', function ($query) use ($productName) {
+                            $query->where('nama_produk', 'like', $productName . '%');
+                        })->whereHas('transaksi', function ($query) use ($date) {
+                            $query->whereDate('tanggal_ambil', $date);
+                            $query->where('status_transaksi', '!=', 'rejected');
+                        })->get()->sum(function ($detailTransaksi) {
+                            if (!Str::contains($detailTransaksi->produk->nama_produk, '1/2')) {
+                                return $detailTransaksi->jumlah_item * 2;
+                            } else {
+                                return $detailTransaksi->jumlah_item;
+                            }
+                        });
+                    if (!Str::contains($this->nama_produk, '1/2')) {
+                        $totalSold = ceil($totalSold / 2);
+                    }
+                } else {
+                    $totalSold = $this->detailTransaksi()
+                        ->whereHas('transaksi', function ($query) use ($date) {
+                            $query->whereDate('tanggal_ambil', $date);
+                            $query->where('status_transaksi', '!=', 'rejected');
+                        })
+                        ->sum('jumlah_item');
+                }
+            } else {
+                $productName = $this->nama_produk;
+                if (Str::contains($this->nama_produk, '1/2')) {
+                    $totalSold =
+                        DetailTransaksi::whereHas('produk', function ($query) use ($productName) {
+                            $query->where('nama_produk', $productName);
+                        })->whereHas('transaksi', function ($query) use ($date) {
+                            $query->whereDate('tanggal_ambil', $date);
+                            $query->where('status_transaksi', '!=', 'rejected');
+                        })->get()->sum(function ($detailTransaksi) {
+                            return $detailTransaksi->jumlah_item;
+                        });
+                    if ($totalSold != 1) {
+                        $totalSold = $this->limit_produksi;
+                    } else {
+                        $totalSold =
+                            $this->limit_produksi - 1;
+                    }
+                } else {
+                    $totalSold = $this->limit_produksi;
+                }
+            }
         }
         $stock = $this->limit_produksi - $totalSold;
 
