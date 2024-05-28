b7ab702addd30d408f08a6e14c4099c9cc61579f
Allv <wijayairawan123@gmail.com>
2024-05-29 01:02:40 +0700

    Feature: LaporanController dan PenggunaanBahanBaku

---
+++ app/Http/Controllers/LaporanController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\PenggunaanBahanBaku;
+
+class LaporanController extends Controller
+{
+    //Todo
+}
--- app/Models/PenggunaanBahanBaku.php
+++ app/Models/PenggunaanBahanBaku.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PenggunaanBahanBaku extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'penggunaan_bahan_baku';
+    protected $primaryKey = 'id_penggunaan_bahan_baku';
+    protected $fillable = [
+        'id_bahan_baku',
+        'jumlah_penggunaan',
+        'tanggal_penggunaan'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    public function bahanBaku()
+    {
+        return $this->belongsTo(BahanBaku::class, 'id_bahan_baku', 'id_bahan_baku');
+    }
 }
